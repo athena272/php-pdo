d935121099698e5cb4d32d7f6f6d01de19fd51fd
athena272 <guilhermera272@gmail.com>
2025-06-05 05:33:06 -0300

    Update select-students.php

--- select-students.php
+++ select-students.php
@@ -3,17 +3,9 @@
 require_once 'vendor/autoload.php';
 
 use Athena272\Pdo\Domain\Models\Student;
+use Athena272\Pdo\Infrastructure\Persistence\ConnectionCreator;
 
-$databasePath = __DIR__ . DIRECTORY_SEPARATOR . 'database.sqlite';
-
-try {
-    $pdo = new PDO("sqlite:$databasePath");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "✅ Successfully connected to the SQLite database." . PHP_EOL;
-} catch (PDOException $e) {
-    echo "❌ Failed to connect to the SQLite database: " . $e->getMessage() . PHP_EOL;
-    exit();
-}
+$pdo = ConnectionCreator::createConnection();
 
 $sqlSelect = "SELECT * FROM students";
 
