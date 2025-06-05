b5986cfe8d217f79ea0f9bbd0ad26a7131787e37
athena272 <guilhermera272@gmail.com>
2025-06-05 05:21:40 -0300

    Update insert-student.php

--- insert-student.php
+++ insert-student.php
@@ -9,7 +9,7 @@ $databasePath = __DIR__ . DIRECTORY_SEPARATOR . 'database.sqlite';
 try {
     $pdo = new PDO("sqlite:$databasePath");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "✅ Successfully connected to the SQLite database.";
+    echo "✅ Successfully connected to the SQLite database." . PHP_EOL;
 } catch (PDOException $e) {
     echo "❌ Failed to connect to the SQLite database: " . $e->getMessage();
     exit();
@@ -17,15 +17,19 @@ try {
 
 $student = new Student(
     null,
-    'Vinicius Dias',
-    new DateTimeImmutable('1997-10-15')
+    'Patricia Freitas',
+    new DateTimeImmutable('1986-10-25')
 );
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?)";
+//$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?)";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date)";
 $statement = $pdo->prepare($sqlInsert);
-$statement->bindValue(1, $student->getName());
-$statement->bindValue(2, $student->getBirthDate()->format('Y-m-d'));
-$statement->execute();
+$statement->bindValue(':name', $student->getName());
+$statement->bindValue(':birth_date', $student->getBirthDate()->format('Y-m-d'));
+if($statement->execute()) {
+    echo "Studend add successfully." . PHP_EOL;
+}
 
 echo $sqlInsert . PHP_EOL;
+
 
 //var_dump($pdo->exec($sqlInsert));
