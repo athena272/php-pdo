df88e664030e19ed8676893fc27c6db434e20ce9
athena272 <guilhermera272@gmail.com>
2025-06-05 05:33:03 -0300

    Create ConnectionCreator.php

---
+++ src/Infrastructure/Persistence/ConnectionCreator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Athena272\Pdo\Infrastructure\Persistence;
+
+use PDO;
+use PDOException;
+
+class ConnectionCreator
+{
+    public static function createConnection(): PDO
+    {
+        $databasePath = __DIR__ . DIRECTORY_SEPARATOR . '../../../database.sqlite';
+
+        try {
+            $pdo = new PDO("sqlite:$databasePath");
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "✅ Successfully connected to the SQLite database." . PHP_EOL;
+            return $pdo;
+        } catch (PDOException $e) {
+            echo "❌ Failed to connect to the SQLite database: " . $e->getMessage();
+            exit();
+        }
+    }
+}
