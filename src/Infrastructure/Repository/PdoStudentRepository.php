9f2db99345ad54e1b2fcd7d4135916035c618633
athena272 <guilhermera272@gmail.com>
2025-06-05 12:38:29 -0300

    Update PdoStudentRepository.php

--- src/Infrastructure/Repository/PdoStudentRepository.php
+++ src/Infrastructure/Repository/PdoStudentRepository.php
@@ -4,10 +4,18 @@ namespace Athena272\Pdo\Infrastructure\Repository;
 
 use Athena272\Pdo\Domain\Models\Student;
 use Athena272\Pdo\Domain\Repository\StudentRepository;
+use Athena272\Pdo\Infrastructure\Persistence\ConnectionCreator;
 use DateTimeInterface;
+use PDO;
 
 class PdoStudentRepository implements StudentRepository
 {
+    private PDO $connection;
+
+    public function __construct()
+    {
+        $this->connection = ConnectionCreator::createConnection();
+    }
 
     public function allStudents(): array
     {
