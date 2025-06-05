7d3e7ea6e92f84c271b2a20d00897a291faae057
athena272 <guilhermera272@gmail.com>
2025-06-05 05:33:11 -0300

    Update delete-student.php

--- delete-student.php
+++ delete-student.php
@@ -2,16 +2,9 @@
 
 require_once 'vendor/autoload.php';
 
-$databasePath = __DIR__ . DIRECTORY_SEPARATOR . 'database.sqlite';
+use Athena272\Pdo\Infrastructure\Persistence\ConnectionCreator;
 
-try {
-    $pdo = new PDO("sqlite:$databasePath");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "✅ Successfully connected to the SQLite database." . PHP_EOL;
-} catch (PDOException $e) {
-    echo "❌ Failed to connect to the SQLite database: " . $e->getMessage();
-    exit();
-}
+$pdo = ConnectionCreator::createConnection();
 
 $sqlDelete = "DELETE FROM students WHERE id = :id";
 $preparedStatement = $pdo->prepare($sqlDelete);
