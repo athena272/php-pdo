246cd2691490b7cffda055ec76e79c9b9633e807
athena272 <guilhermera272@gmail.com>
2025-06-08 23:52:29 -0300

    Update create-class.php

--- create-class.php
+++ create-class.php
@@ -10,18 +10,21 @@ $connection = ConnectionCreator::createConnection();
 $studentRepository = new PdoStudentRepository($connection);
 
 $connection->beginTransaction();
-$nicoSteppat = new Student(
-    null,
-    'Nico Steppat',
-    new DateTimeImmutable('1985-05-01')
-);
-$studentRepository->save($nicoSteppat);
-$samanthaLopes = new Student(
-    null,
-    'Samantha Lopes',
-    new DateTimeImmutable('1985-05-01')
-);
-$studentRepository->save($samanthaLopes);
-
-$connection->rollBack();
-//$connection->commit();
+try {
+    $nicoSteppat = new Student(
+        null,
+        'Nico Steppat',
+        new DateTimeImmutable('1985-05-01')
+    );
+    $studentRepository->save($nicoSteppat);
+    $samanthaLopes = new Student(
+        null,
+        'Samantha Lopes',
+        new DateTimeImmutable('1985-05-01')
+    );
+    $studentRepository->save($samanthaLopes);
+    $connection->commit();
+} catch (RuntimeException $e) {
+    echo $e->getMessage() . PHP_EOL;
+    $connection->rollBack();
+}
