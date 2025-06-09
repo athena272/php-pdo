c7049e0bc846a95cd776ea0fad3cf53a6eb77012
athena272 <guilhermera272@gmail.com>
2025-06-09 07:56:18 -0300

    Update select-students.php

--- select-students.php
+++ select-students.php
@@ -2,26 +2,12 @@
 
 require_once 'vendor/autoload.php';
 
-use Athena272\Pdo\Domain\Models\Student;
 use Athena272\Pdo\Infrastructure\Persistence\ConnectionCreator;
+use Athena272\Pdo\Infrastructure\Repository\PdoStudentRepository;
 
 $pdo = ConnectionCreator::createConnection();
+$repository = new PdoStudentRepository($pdo);
+$studentList = $repository->allStudents();
 
-$sqlSelect = "SELECT * FROM students";
-
-$statement = $pdo->query($sqlSelect);
-while ($studentData = $statement->fetch(PDO::FETCH_ASSOC)) {
-    try {
-        $student = new Student(
-            $studentData['id'],
-            $studentData['name'],
-            new DateTimeImmutable($studentData['birth_date'])
-        );
-
-        echo $student->age() . PHP_EOL;
-    } catch (DateMalformedStringException $e) {
-        echo "❌ Error: " . $e->getMessage() . PHP_EOL;
-    }
-}
-var_dump($statement->fetchColumn(1));
+var_dump($studentList);
 
